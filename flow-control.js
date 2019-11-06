2c6832eb4a2daf5cc0276d486b191e03b19dd114
Finny Abraham <kozakk@gmail.com>
2019-11-06 00:22:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -33,8 +33,22 @@ function ternaryTeenager(age) {
   if (age>=13) {if (age <=19) {
     return "You are a teenager"
   }}
+  if (age<13) {
+    return "You are not a teenager"
+  }
+  if (age>19) {
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
-
+  if (age>=13) {if (age <=19) {
+    return "You are a teenager"
+  }}
+  if (age<13) {
+    return "You have an age"
+  }
+  if (age>19) {
+    return "You have an age"
+  }
 }
